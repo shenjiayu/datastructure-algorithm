b0265253e51592d1981bb392279da7fd8f9c5791
shenjiayu <sjy19930312@gmail.com>
2016-02-05 16:08:14 +0800

    Longest Increasing Subsequence

---
+++ LongestIncreasingSubsequence.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int binarySearch(int c[], int n, int target) {
+	int left = 1, right = n;
+    while(left <= right){
+        int mid = (left + right)/2;
+        if(c[mid] > target)
+            right = mid - 1;
+        else if(c[mid] < target)
+            left = mid + 1;
+        else 
+            return mid;
+    }
+    return left;
+}
+
+int lengthOfLIS(int* nums, int numsSize) {
+	const int len = numsSize;
+    if(len <= 1)  return len;
+    int c[len+1],ret = 1;
+    for(int i = 0; i <= len; i++){
+         c[i] = 0;
+    }
+    c[1] = nums[0];
+    for(int i = 1; i < len; i++){
+        int temp = binarySearch(c, ret, nums[i]);
+        c[temp] = nums[i];
+        if(ret < temp)  ret = temp;
+    }
+    return ret;
+}
+
+int main() {
+	int nums[] = {10, 9, 2, 5, 3, 7, 101, 18};
+	int length = lengthOfLIS(nums, 8);
+	printf("%d\n", length);
+	return 0;
+}
